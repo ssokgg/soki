4eecb9276437ea6b7f61975470bdf1a62ced95d1
sandra sok <sandra.sok@gitguardian.com>
2023-08-03 10:17:18 +0200

    lala

---
+++ toto/magic copy 2.py
@@ -0,0 +1,1 @@
+database = aws_lib.connect("AKIAF6BAFJKR65SBWVB1", "nhxhnk5ex5u34565AWS74/JKGjhz545d89sjkje")
---
+++ toto/magic copy.py
@@ -0,0 +1,1 @@
+database = aws_lib.connect("AKIAF6BAFJKR65SBWVB1", "tgwhnk5ex5u34565AWS74/JKGjhz545d89sjkje")
